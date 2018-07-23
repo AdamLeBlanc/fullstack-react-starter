d26e03907a5c19c85df974fdbbe3f13f839ffd05
Adam LeBlanc <leblanca37@gmail.com>
2018-07-23 14:26:15 -0300

    Added redis for session storage

--- server/src/middleware/session-middleware.js
+++ server/src/middleware/session-middleware.js
@@ -1,5 +1,5 @@
 const session = require('express-session');
-
+const RedisStore = require('connect-redis')(session);
 module.exports = session({
   secret: 'shhhhhhh',
   resave: false,
@@ -10,4 +10,8 @@ module.exports = session({
     maxAge: 1000 * 60 * 60 * 7,
     sameSite: true,
   },
+  store: new RedisStore({
+    host: 'localhost',
+    port: '6379',
+  }),
 });
